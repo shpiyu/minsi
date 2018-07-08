ef925f68609196d4a6c22a6be7c0f74426a4123c
Piyush Ranjan <piyushranjan95@gmail.com>
2018-07-08 13:46:06 +0530

    create navbar skeleton and style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 
--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public onSearch(cusip:string) {
+    console.log(cusip);
+  }
+
+  public onRefresh() {
+    console.log("on refresh clicked");
   }
 
 }
